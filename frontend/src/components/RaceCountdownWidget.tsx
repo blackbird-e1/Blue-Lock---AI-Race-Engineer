8f5dba82fd420e5232b810d0e33369349607ab9c
subhrajit95 <subhrajitshome95@gmail.com>
2026-06-06 22:23:39 +0530

    Fixed a frontend bug regarding live tracker view

--- frontend/src/components/RaceCountdownWidget.tsx
+++ frontend/src/components/RaceCountdownWidget.tsx
@@ -44,19 +44,19 @@ export default function RaceCountdownWidget() {
       return;
     }
 
-    const days = Math.floor(
-      diff / (1000 * 60 * 60 * 24)
-    );
+    // const days = Math.floor(
+    //   diff / (1000 * 60 * 60 * 24)
+    // );
 
-    const hours = Math.floor(
-      (diff % (1000 * 60 * 60 * 24))
-      / (1000 * 60 * 60)
-    );
+    // const hours = Math.floor(
+    //   (diff % (1000 * 60 * 60 * 24))
+    //   / (1000 * 60 * 60)
+    // );
 
-    const minutes = Math.floor(
-      (diff % (1000 * 60 * 60))
-      / (1000 * 60)
-    );
+    // const minutes = Math.floor(
+    //   (diff % (1000 * 60 * 60))
+    //   / (1000 * 60)
+    // );
 
     // setCountdown(
     //   `${days}D ${hours}H ${minutes}M`
