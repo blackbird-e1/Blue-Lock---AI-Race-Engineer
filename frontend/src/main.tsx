f61602fd2acf65700bd5defa044c0ad718de8755
subhrajit95 <subhrajitshome95@gmail.com>
2026-06-09 15:31:15 +0530

    changed the main.tsx regarding the dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App.tsx';
 
+document.documentElement.classList.add('dark');
+
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById('root')!).render(
